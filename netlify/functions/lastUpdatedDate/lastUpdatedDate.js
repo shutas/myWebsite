77559ebeb4131efe6ef70919dd00e08180fa24c8
しゅーたす <shutas@umich.edu>
2024-09-03 22:18:21 +0900

    MOD: Changed format of scraped date string

--- netlify/functions/lastUpdatedDate/lastUpdatedDate.js
+++ netlify/functions/lastUpdatedDate/lastUpdatedDate.js
@@ -1,5 +1,26 @@
 const url = 'https://github.com/shutas/myWebsite/commits/main/';
 
+const modifyDateString = (oldDateString) => {
+  const newDate = new Date(oldDateString);
+  const year = newDate.getFullYear();
+  const month = newDate.toLocaleString("default", { month: "long" });
+  const day = newDate.getDate();
+  const nthNumber = (number) => {
+    if (number > 3 && number < 21) return "th";
+    switch (number % 10) {
+      case 1:
+        return "st";
+      case 2:
+        return "nd";
+      case 3:
+        return "rd";
+      default:
+        return "th";
+    }
+  };
+  return `${month} ${day}${nthNumber(day)}, ${year}`;
+}
+
 const handler = async (event) => {
   try {
     const response = await fetch(url);
@@ -11,7 +32,7 @@ const handler = async (event) => {
     
     return {
       statusCode: 200,
-      body: JSON.stringify({ value: lastUpdatedDateStr }),
+      body: JSON.stringify({ value: modifyDateString(lastUpdatedDateStr) }),
     }
   } catch(error) {
     const errorMessage = "oops, something went very wrong... please try again later";
